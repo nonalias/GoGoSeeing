10bbaf4b011736b882f3170d780d0c662ac0e8be
nonalias <ktw1532@naver.com>
2019-06-17 22:01:45 +0900

    상세보기 구현

--- www/exhibitDisplay.js
+++ www/exhibitDisplay.js
@@ -1,42 +1,42 @@
-// 맛집 상세 정보 표시
-function displayCafeInfo(index) {
+// 전시회 상세 정보 표시
+function displayExhibitInfo(index) {
   var len, i, name="", type="", region="", phone="", address="", memo="", pic="";
 
-  // 맛집 상세 정보를 설정
-  var myCafeRecord = recordSet.rows.item(index);    
+  // 전시회 상세 정보를 설정
+  var myExhibitRecord = recordSet.rows.item(index);    
   varPosition = index;
   
-  if(myCafeRecord.name != null) {          // 맛집 이름           
-    name = '<div class="ui-bar ui-bar-a"><h3>' + myCafeRecord.name + '</h3></div>';   
+  if(myExhibitRecord.name != null) {          // 맛집 이름           
+    name = '<div class="ui-bar ui-bar-a"><h3>' + myExhibitRecord.name + '</h3></div>';   
   } else {
       name = '<p>이름 : 정보없음</p>';
   }  
-  if(myCafeRecord.name != null) {          // 유형 
-      type = '<p>유형 : ' + myCafeRecord.type + '</p>';
+  if(myExhibitRecord.name != null) {          // 유형 
+      type = '<p>유형 : ' + myExhibitRecord.type + '</p>';
   } else {
       type = '<p>유형 : 정보없음</p>';
   }   
-  if(myCafeRecord.region != null) {          // 지역 
-      region = '<p>지역 : ' + myCafeRecord.region + '</p>';            
+  if(myExhibitRecord.region != null) {          // 지역 
+      region = '<p>지역 : ' + myExhibitRecord.region + '</p>';            
   } else {
       region = '<p>지역 : 정보없음</p>';
   }   
-  if(myCafeRecord.phone != null) {          // 전화번호 
-      phone = '<p>전화 : ' + myCafeRecord.phone + '</p>';            
+  if(myExhibitRecord.phone != null) {          // 전화번호 
+      phone = '<p>전화 : ' + myExhibitRecord.phone + '</p>';            
   } else {
       phone = '<p>전화 : 정보없음</p>';
   }   
-  if(myCafeRecord.address != null) {          // 주소 
-      address = '<p>주소 : ' + myCafeRecord.address + '</p>';   
+  if(myExhibitRecord.address != null) {          // 주소 
+      address = '<p>주소 : ' + myExhibitRecord.address + '</p>';   
   } else {
       address = '<p>주소 : 정보없음</p>';
   }  
-  if(myCafeRecord.memo != null) {          // 메모  
-      memo = '<p>메모 : ' + myCafeRecord.memo + '</p>';   
+  if(myExhibitRecord.memo != null) {          // 메모  
+      memo = '<p>메모 : ' + myExhibitRecord.memo + '</p>';   
   } else {
       memo = '<p>메모 : 정보없음</p>';
   }  
                              
-  $('#exhibitInfoArea').html(name + type + score + region + phone + address + memo);
+  $('#exhibitInfoArea').html(name + type + region + phone + address + memo);
   $.mobile.changePage("#exhibitInfoShowPage", "slide", false, true);
 }
