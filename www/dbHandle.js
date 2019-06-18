f9f46fdd60e69d30f777bcae0f1347659dd1e733
nonalias <ktw1532@naver.com>
2019-06-18 16:20:14 +0900

    일정완성

--- www/dbHandle.js
+++ www/dbHandle.js
@@ -7,7 +7,7 @@ function openDB() {
 // 테이블 생성 트랜잭션 실행
 function createTable() {
 	db.transaction(function(tr) {
-		var createSQL = 'create table if not exists exhibit(id integer primary key autoincrement, name varchar(20) not null unique, type varchar(20), region varchar(20), phone varchar(20), address varchar(30), memo varchar(200), pic varchar(50), sDate date(10), eDate date(10))';
+		var createSQL = 'create table if not exists exhibit(id integer primary key autoincrement, name varchar(20) not null unique, type varchar(20), region varchar(20), phone varchar(20), address varchar(30), memo varchar(200), pic varchar(50), sDate date(10), eDate date(10), checked boolean(1) default false)';
 
 		tr.executeSql(createSQL, [], function() {
 			console.log('2_1_테이블생성_sql 실행 성공...');
@@ -100,3 +100,56 @@ function deleteExhibit() {
 		alert("취소하셨습니다.");
 	}
 }
+
+
+
+// 선택한 일정을 찾아서 나열해줌
+function selectPlanList() {
+	$('#planExhibitListArea').listview('refresh');
+	db.transaction(function(tr) {
+		var i,
+		    count,
+		    tagList = '';
+		var ischecked=1;
+		// var sType = $('#exhibitType3').val();
+		// var sRegion = $('#exhibitRegion3').val();
+		// type like ? and region like ?
+		var selectSQL = 'select name, type, region, phone, address, memo, pic, sDate, eDate  from exhibit where checked=?';
+		tr.executeSql(selectSQL, [ischecked], function(tr, rs) {
+			console.log(' 전시회 조회... ' + rs.rows.length + '건.');
+			recordSetSelected = rs;
+			count = rs.rows.length;
+			if (count > 0) {
+				tagList = '<li data-role="list-divider">전시회 목록' + '<span style="float:right">' + count + '건' + '</span></li>';
+				for ( i = 0; i < count; i += 1) {
+					tagList += '<li><a onclick="displayExhibitInfo(' + i + ');">';
+					tagList += '<img class="my_listview_img" src="' + rs.rows.item(i).pic + '">';
+					tagList += '<h2>' + rs.rows.item(i).name + '</h2>';
+					tagList += '<p>' + rs.rows.item(i).type + '</p>';
+					tagList += '<p>' + rs.rows.item(i).address + '</p></a></li>';
+				}
+				$('#planExhibitListArea').html(tagList);
+				$('#planExhibitListArea').listview('refresh');
+			} else {
+				navigator.notification.alert('검색 결과 없음', null, '맛집 검색 실패');
+			}
+		});
+	});
+}
+
+
+
+// 내 일정에 저장
+ function setDate(){
+    db.transaction(function(tr){
+        var name = varExhibitName;      
+        var updateSQL = 'update exhibit set checked=true where name = ?';          
+        tr.executeSql(updateSQL, [name], function(tr, rs){    
+             console.log('5_일정에 저장 .... ') ;
+             alert('전시회 명 ' + varExhibitName + ' 이(가) 일정에 등록되었습니다');                      
+
+        }, function(){
+            alert('전시회 명 ' + varExhibitName + ' 이(가) 일정 등록에 실패 하였습니다');                  	 
+        });
+    });       
+ }
