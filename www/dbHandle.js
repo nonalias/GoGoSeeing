65010761af772152f08fff4292d7c314114ee15d
nonalias <ktw1532@naver.com>
2019-06-18 15:06:49 +0900

    메모까지 완료

--- www/dbHandle.js
+++ www/dbHandle.js
@@ -1,94 +1,102 @@
 // 데이터베이스 생성 및 열기
-function openDB(){
-   db = window.openDatabase('exhibitDB', '1.0', '전시회DB', 1024*1024);
-   console.log('1_DB 생성...');
+function openDB() {
+	db = window.openDatabase('exhibitDB', '1.0', '전시회DB', 1024 * 1024);
+	console.log('1_DB 생성...');
 }
 
 // 테이블 생성 트랜잭션 실행
- function createTable() {
-    db.transaction(function(tr){
-        var createSQL = 'create table if not exists exhibit(id integer primary key autoincrement, name varchar(20) not null unique, type varchar(20), region varchar(20), phone varchar(20), address varchar(30), memo varchar(200), pic varchar(50), sDate date(10), eDate date(10))';
+function createTable() {
+	db.transaction(function(tr) {
+		var createSQL = 'create table if not exists exhibit(id integer primary key autoincrement, name varchar(20) not null unique, type varchar(20), region varchar(20), phone varchar(20), address varchar(30), memo varchar(200), pic varchar(50), sDate date(10), eDate date(10))';
 
-       tr.executeSql(createSQL, [], function(){
-          console.log('2_1_테이블생성_sql 실행 성공...');
-       }, function(){
-          console.log('2_1_테이블생성_sql 실행 실패...');
-       });
-    }, function(){
-                console.log('2_2_테이블 생성 트랜잭션 실패...롤백은 자동');
-    }, function(){
-                console.log('2_2_테이블 생성 트랜잭션 성공...');
-    });
- }
+		tr.executeSql(createSQL, [], function() {
+			console.log('2_1_테이블생성_sql 실행 성공...');
+		}, function() {
+			console.log('2_1_테이블생성_sql 실행 실패...');
+		});
+	}, function() {
+		console.log('2_2_테이블 생성 트랜잭션 실패...롤백은 자동');
+	}, function() {
+		console.log('2_2_테이블 생성 트랜잭션 성공...');
+	});
+}
 
- // 데이터 입력 트랜잭션 실행
- function insertExhibit(){
-    db.transaction(function(tr){
-        var name = $('#exhibitName1').val();
-        var type = $('#exhibitType1').val();
-        var region = $('#exhibitRegion1').val();
-        var phone = $('#exhibitPhone1').val();
-        var address = $('#exhibitAddress1').val();
-        var memo = $('#exhibitMemo1').val();
-        var pic = $('#exhibitPic1').val();
-        var sDate = $('#exhibitStartDate').val();
-        var eDate = $('#exhibitEndDate').val();
-        var insertSQL = 'insert into exhibit(name, type, region, phone, address, memo, pic, sDate, eDate) values(?,?,?,?,?,?,?,?,?)';
-        tr.executeSql(insertSQL, [name, type, region, phone, address, memo, pic, sDate, eDate], function(tr, rs){
-             console.log('3_ 전시회 등록...no: ' + rs.insertId);
-             alert('전시회 명 ' + $('#exhibitName1').val() + ' 이(가) 입력되었습니다');
-             $('#exhibitType1').val('미정').attr('selected', 'selected');
-             $('#exhibitType1').selectmenu('refresh');
-             $('#exhibitRegion1').val('미정').attr('selected', 'selected');
-             $('#exhibitRegion1').selectmenu('refresh');
+// 데이터 입력 트랜잭션 실행
+function insertExhibit() {
+	db.transaction(function(tr) {
+		var name = $('#exhibitName1').val();
+		var type = $('#exhibitType1').val();
+		var region = $('#exhibitRegion1').val();
+		var phone = $('#exhibitPhone1').val();
+		var address = $('#exhibitAddress1').val();
+		var memo = $('#exhibitMemo1').val();
+		var pic = $('#exhibitPic1').val();
+		var sDate = $('#exhibitStartDate').val();
+		var eDate = $('#exhibitEndDate').val();
+		var insertSQL = 'insert into exhibit(name, type, region, phone, address, memo, pic, sDate, eDate) values(?,?,?,?,?,?,?,?,?)';
+		tr.executeSql(insertSQL, [name, type, region, phone, address, memo, pic, sDate, eDate], function(tr, rs) {
+			console.log('3_ 전시회 등록...no: ' + rs.insertId);
+			alert('전시회 명 ' + $('#exhibitName1').val() + ' 이(가) 입력되었습니다');
+			$('#exhibitType1').val('미정').attr('selected', 'selected');
+			$('#exhibitType1').selectmenu('refresh');
+			$('#exhibitRegion1').val('미정').attr('selected', 'selected');
+			$('#exhibitRegion1').selectmenu('refresh');
 
-        	 form1.reset();
-        }, function(){
-            alert('전시회 명 ' + $('#exhibitName1').val() + ' 이(가) 입력 실패하였습니다');
-         });
-    });
- }
- 
-  // 전시회 목록 동적 구성을 위한 데이터 검색 트랜잭션 실행
+			form1.reset();
+		}, function() {
+			alert('전시회 명 ' + $('#exhibitName1').val() + ' 이(가) 입력 실패하였습니다');
+		});
+	});
+}
+
+// 전시회 목록 동적 구성을 위한 데이터 검색 트랜잭션 실행
 function selectExhibitList() {
-  	db.transaction(function(tr){
-		var i, count, tagList='';   
-	    var	sType = $('#exhibitType3').val();  	 
-	    var	sRegion = $('#exhibitRegion3').val();  
-	    var selectSQL = 'select name, type, region, phone, address, memo, pic, sDate, eDate  from exhibit where type like ? and region like ?'; 	     	
-	  	tr.executeSql(selectSQL, [sType, sRegion], function(tr, rs){    
-   			console.log(' 전시회 조회... ' + rs.rows.length + '건.');  
-   			recordSet = rs;
+	$('#exhibitListArea').listview('refresh');
+	db.transaction(function(tr) {
+		var i,
+		    count,
+		    tagList = '';
+		var sType = $('#exhibitType3').val();
+		var sRegion = $('#exhibitRegion3').val();
+		var selectSQL = 'select name, type, region, phone, address, memo, pic, sDate, eDate  from exhibit where type like ? and region like ?';
+		tr.executeSql(selectSQL, [sType, sRegion], function(tr, rs) {
+			console.log(' 전시회 조회... ' + rs.rows.length + '건.');
+			recordSet = rs;
 			count = rs.rows.length;
-			if(count > 0) {
-				tagList = '<li data-role="list-divider">전시회 목록' + '<span style="float:right">'+count+'건'+'</span></li>';				
-			    for( i = 0; i < count; i += 1) {			
+			if (count > 0) {
+				tagList = '<li data-role="list-divider">전시회 목록' + '<span style="float:right">' + count + '건' + '</span></li>';
+				for ( i = 0; i < count; i += 1) {
 					tagList += '<li><a onclick="displayExhibitInfo(' + i + ');">';
-					tagList += '<img class="my_listview_img" src="' + rs.rows.item(i).pic + '">';              					
+					tagList += '<img class="my_listview_img" src="' + rs.rows.item(i).pic + '">';
 					tagList += '<h2>' + rs.rows.item(i).name + '</h2>';
 					tagList += '<p>' + rs.rows.item(i).type + '</p>';
-					tagList += '<p>' + rs.rows.item(i).address + '</p></a></li>';					
-			    }	
-			    $('#exhibitListArea').html(tagList);
-			    $('#exhibitListArea').listview('refresh');
+					tagList += '<p>' + rs.rows.item(i).address + '</p></a></li>';
+				}
+				$('#exhibitListArea').html(tagList);
+				$('#exhibitListArea').listview('refresh');
 			} else {
-			  	navigator.notification.alert('검색 결과 없음', null, '맛집 검색 실패');
+				navigator.notification.alert('검색 결과 없음', null, '맛집 검색 실패');
 			}
 		});
 	});
 }
 
-  // 데이터 삭제 트랜잭션 실행
- function deleteExhibit(){
-   db.transaction(function(tr){
-	  var name = varExhibitName;   
- 	  var deleteSQL = 'delete from exhibit where name = ?';      
-	  tr.executeSql(deleteSQL, [name], function(tr, rs){    
-	     console.log('6_전시회 삭제... ');   
-	     alert('전시회 명 ' + name + ' 이(가) 삭제되었습니다');  
-	     $.mobile.changePage('#mainPage','slide',false,true); 	       
-      }, function(){
-          alert('맛집명 ' + name + ' 이(가) 삭제 실패하였습니다');                   	 
-	  });
-   });         
- } 
+// 데이터 삭제 트랜잭션 실행
+function deleteExhibit() {
+	if (confirm("정말로 삭제하시겠습니까?")) {
+		db.transaction(function(tr) {
+			var name = varExhibitName;
+			var deleteSQL = 'delete from exhibit where name = ?';
+			tr.executeSql(deleteSQL, [name], function(tr, rs) {
+				console.log('6_전시회 삭제... ');
+				alert('전시회 명 ' + name + ' 이(가) 삭제되었습니다');
+				$.mobile.changePage('#mainPage', 'slide', false, true);
+			}, function() {
+				alert('맛집명 ' + name + ' 이(가) 삭제 실패하였습니다');
+			});
+		});
+	}
+	else{
+		alert("취소하셨습니다.");
+	}
+}
--- www/exhibitDisplay.js
+++ www/exhibitDisplay.js
@@ -1,65 +1,105 @@
-
-
 // 전시회 상세 정보 표시
 function displayExhibitInfo(index) {
-  var len, i, name="", type="", region="", phone="", address="", memo="", pic="", sDate="", eDate="";
+	var len,
+	    i,
+	    name = "",
+	    type = "",
+	    region = "",
+	    phone = "",
+	    address = "",
+	    memo = "",
+	    pic = "",
+	    sDate = "",
+	    eDate = "";
 
-  // 전시회 상세 정보를 설정
-  var myExhibitRecord = recordSet.rows.item(index);    
-  varPosition = index;
-  
-  if(myExhibitRecord.name != null) {          // 맛집 이름           
-    name = '<div class="ui-bar ui-bar-a"><h3>' + myExhibitRecord.name + '</h3></div>'; 
-    varExhibitName = myExhibitRecord.name;  
-  } else {
-      name = '<p>이름 : 정보없음</p>';
-  }  
-  if(myExhibitRecord.name != null) {          // 유형 
-      type = '<p>유형 : ' + myExhibitRecord.type + '</p>';
-  } else {
-      type = '<p>유형 : 정보없음</p>';
-  }   
-  if(myExhibitRecord.region != null) {          // 지역 
-      region = '<p>지역 : ' + myExhibitRecord.region + '</p>';            
-  } else {
-      region = '<p>지역 : 정보없음</p>';
-  }   
-  if(myExhibitRecord.phone != null) {          // 전화번호 
-      phone = '<p>전화 : ' + myExhibitRecord.phone + '</p>';    
-      myPhone = myExhibitRecord.phone;        
-  } else {
-      phone = '<p>전화 : 정보없음</p>';
-  }   
-  if(myExhibitRecord.address != null) {          // 주소 
-      address = '<p>주소 : ' + myExhibitRecord.address + '</p>';   
-  } else {
-      address = '<p>주소 : 정보없음</p>';
-  }  
-  if(myExhibitRecord.memo != null) {          // 메모  
-      memo = '<p>메모 : ' + myExhibitRecord.memo + '</p>';   
-  } else {
-      memo = '<p>메모 : 정보없음</p>';
-  }
-  if(myExhibitRecord.sDate != null) {          // 메모  
-      sDate = '<p>시작날짜 : ' + myExhibitRecord.sDate + '</p>';   
-  } else {
-      sDate = '<p>시작날짜 : 정보없음</p>';
-  }
-  if(myExhibitRecord.eDate != null) {          // 메모  
-      eDate = '<p>종료날짜 : ' + myExhibitRecord.eDate + '</p>';   
-  } else {
-      eDate = '<p>종료날짜 : 정보없음</p>';
-  }  
-                             
-  $('#exhibitInfoArea').html(name + type + region + phone + address + memo + sDate + eDate);
-  $.mobile.changePage("#exhibitInfoShowPage", "slide", false, true);
+	// 전시회 상세 정보를 설정
+	var myExhibitRecord = recordSet.rows.item(index);
+	varPosition = index;
+
+	if (myExhibitRecord.name != null) {// 맛집 이름
+		name = '<div class="ui-bar ui-bar-a"><h3>' + myExhibitRecord.name + '</h3></div>';
+		varExhibitName = myExhibitRecord.name;
+	} else {
+		name = '<p>이름 : 정보없음</p>';
+	}
+	if (myExhibitRecord.name != null) {// 유형
+		type = '<p>유형 : ' + myExhibitRecord.type + '</p>';
+	} else {
+		type = '<p>유형 : 정보없음</p>';
+	}
+	if (myExhibitRecord.region != null) {// 지역
+		region = '<p>지역 : ' + myExhibitRecord.region + '</p>';
+	} else {
+		region = '<p>지역 : 정보없음</p>';
+	}
+	if (myExhibitRecord.phone != null) {// 전화번호
+		phone = '<p>전화 : ' + myExhibitRecord.phone + '</p>';
+		myPhone = myExhibitRecord.phone;
+	} else {
+		phone = '<p>전화 : 정보없음</p>';
+	}
+	if (myExhibitRecord.address != null) {// 주소
+		address = '<p>주소 : ' + myExhibitRecord.address + '</p>';
+	} else {
+		address = '<p>주소 : 정보없음</p>';
+	}
+	if (myExhibitRecord.memo != null) {// 메모
+		memo = '<p>메모 : <div class="ui-bar ui-bar-a">' + myExhibitRecord.memo + '</div></p>';
+	} else {
+		memo = '<p>메모 : 정보없음</p>';
+	}
+	if (myExhibitRecord.sDate != null) {// 시작날찌
+		sDate = '<p>시작날짜 : ' + myExhibitRecord.sDate + '</p>';
+	} else {
+		sDate = '<p>시작날짜 : 정보없음</p>';
+	}
+	if (myExhibitRecord.eDate != null) {// 종료날짜
+		eDate = '<p>종료날짜 : ' + myExhibitRecord.eDate + '</p>';
+	} else {
+		eDate = '<p>종료날짜 : 정보없음</p>';
+	}
+
+	$('#exhibitInfoArea').html(name + type + region + phone + address + sDate + eDate + memo);
+	$.mobile.changePage("#exhibitInfoShowPage", "slide", false, true);
 }
 
-
 function getExhibitPic() {
-  var myName = recordSet.rows.item(varPosition).name; 
-  var myPic = recordSet.rows.item(varPosition).pic;   
-  $('#picName').text(myName);          
-  $('#picArea').attr('src', myPic);
-  $.mobile.changePage("#picShowDialog", "pop", false, true);            
+	var myName = recordSet.rows.item(varPosition).name;
+	var myPic = recordSet.rows.item(varPosition).pic;
+	$('#picName').text(myName);
+	$('#picArea').attr('src', myPic);
+	$.mobile.changePage("#picShowDialog", "pop", false, true);
+}
+
+function getExhibitRouteMap() {
+	var myName = recordSet.rows.item(varPosition).address;
+	$('#routeName').text(myName);
+	$.mobile.changePage("#routeShowDialog", "pop", false, true);
+	$('#routeArea').gmap('destroy');
+	navigator.geolocation.getCurrentPosition(function(Position) {
+		var lat = Position.coords.latitude;
+		var lng = Position.coords.longitude;
+
+		var start = new google.maps.LatLng(lat, lng);
+		var end = recordSet.rows.item(varPosition).address;
+		var mode = 'TRANSIT';
+		var request = {
+			origin : start,
+			destination : end,
+			travelMode : eval('google.maps.DirectionsTravelMode.' + mode)
+		};
+		$('#routeArea').gmap('displayDirections', request, function(result, status) {
+			if (status === 'OK') {
+				alert('성공');
+			} else {
+				alert('실패 : ' + status);
+			}
+		});
+	}, function(posError) {
+		alert('Error Code : ' + posError.code + ' / Error Message : ' + posError.message);
+	}, {
+		maximumAge : 3000,
+		timeout : 50000,
+		enableHighAccuracy : true
+	});
 }
